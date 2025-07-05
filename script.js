5e2140d9a2a932cac0de7a81e7a0ea832cb2641b
Vikeant38 <aryan4763.0@gmail.com>
2025-07-05 15:22:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ async function DisplayAblums(params) {
     let array = Array.from(anchors)
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
-        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
+        if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
     
             //Get the meta data of folders  
